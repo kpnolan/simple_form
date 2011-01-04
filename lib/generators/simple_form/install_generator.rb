d5cf43492c2a869b5c65d5d02c773785e02eb160
Kevin Nolan <kpnolan@comcast.net>
2011-01-03 18:14:38 -0800

    added template files for _rows, index, new form of show (virtually the same as edit only with everything disables and stylized as so)

--- lib/generators/simple_form/install_generator.rb
+++ lib/generators/simple_form/install_generator.rb
@@ -12,6 +12,18 @@ module SimpleForm
       def copy_locale_file
         copy_file 'en.yml', 'config/locales/simple_form.en.yml'
       end
+      
+      def copy_index_files
+        engine = options[:template_engine]
+        copy_file "index.html.#{engine}", "lib/templates/#{engine}/scaffold/index.html.#{engine}"
+        copy_file "_row.html.#{engine}", "lib/templates/#{engine}/scaffold/_row.html.#{engine}"
+      end
+      
+      def copy_alternative_show_file
+        engine = options[:template_engine]
+        copy_file "show.html.#{engine}", "lib/templates/#{engine}/scaffold/show.html.#{engine}"
+      end
+        
 
       def copy_scaffold_template
         engine = options[:template_engine]
